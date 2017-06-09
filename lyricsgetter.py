ce52ffa1e7be9ef71311d588e6e83240709c2de0
Ichicoro <ichicoro@users.noreply.github.com>
2017-06-09 14:46:59 +0200

    Switched to Genius. Using geniuslyrics as the library to scrape data from Genius. It works!

--- lyricsgetter.py
+++ lyricsgetter.py
@@ -1,11 +1,43 @@
-from PyLyrics import *
+import json, os
 
-def getLyrics(songartist, songname):
-    try:
-        return PyLyrics.getLyrics(songartist,songname)  # Try returning the lyrics directly
-    except ValueError:
-        return "lyrics not found"                       # We might not find anything tho D:
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import requests
 
 
-if __name__ == "__main__":
-    print(getLyrics("Dragonforce", "Through the fire and flames"))  # On a cold winter morning...
+class geniuslyrics:
+    def __init__(self, client_access_token):
+        self.client_access_token = client_access_token
+
+
+    # use the Genius API to search for a song and retrieve the URL
+    def search(self, song_query):
+        # Generate search request
+        response = requests.get("http://api.genius.com/search?access_token=" + self.client_access_token + "&q=" + song_query , headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+        search_json = json.loads(response.text)
+        print(search_json)
+        song_url = search_json['response']['hits'][0]['result']['url']
+        
+        return song_url
+
+    # scrape a Genius song page for the full lyrics
+    def scrape(self, song_url):
+        print("Retrieving lyrics from " + song_url)
+
+        # Get raw lyrics for first song result
+        response = requests.get(song_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+
+        # Clean up html
+        soup = BeautifulSoup(response.text, "html.parser")
+        soup = soup.find("div", class_="lyrics")
+        lyrics = soup.get_text().strip()
+
+        return lyrics
+
+
+def getSongByName(song_name):
+    # genius = geniuslyrics(CLIENT_ACCESS_TOKEN)
+    gl = geniuslyrics(os.environ["GENIUS_ACCESS_TOKEN"])
+    result = gl.scrape(gl.search(song_name))
+    print(result)
+    return result
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from telegram.ext import Updater, CommandHandler, Filters
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 import logging
-import lyricsgetter
+import lyricsgetter 
+import os
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, 
-        text="Hello! With this bot you'll be able to find lyrics for your favourite songs :) Please type the name of the artist and of the song you wanna search the lyrics for, separated by a comma (eg: dragonforce, through the fire and flames).")
+        text="Hello! With this bot you'll be able to find lyrics for your favourite songs :) Please type the name of the artist and of the song you wanna search the lyrics for, separated by a comma (eg: through the fire and flames,dragonforce).")
 
-def msgHandler(bot, update):
-    songdata = update.message.text.split(',')
+
+def tryToFindLyrics():
     response = getLyrics(songdata[0],songdata[1])
     if response != "lyrics not found":
         bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
-        bot.send_message(chat_id=update.message.chat_id, text="Lyrics not found :(")
+        bot.send_message(chat_id=update.message.chat_id, text="Lyrics not found :( Try with a different query")
+
+
+def msgHandler(bot, update):
+    print(update.message.text)
+    print("It's a song!")
+    bot.send_message(chat_id=update.message.chat_id, 
+        text=lyricsgetter.getSongByName(update.message.text.strip()))  
 
 
 if __name__=="__main__":
-    updater = Updater(token='DUMMY_API_KEY')    # heh, I still need an API key :(
-    dispatcher = updater.dispatcher             # setting up the dispatcher for easier access
+    print("LyrxBot v0.1")
+
+    if "TELEGRAM_ACCESS_TOKEN" not in os.environ:
+        print("TELEGRAM_ACCESS_TOKEN environment variable not set. Exiting!")
+        exit()
+
+    if "GENIUS_ACCESS_TOKEN" not in os.environ:
+        print("GENIUS_ACCESS_TOKEN environment variable not set. Exiting!")
+        exit()
+
+    updater = Updater(token=os.environ["TELEGRAM_ACCESS_TOKEN"])    # reads from the environment variables!
+    dispatcher = updater.dispatcher                                 # setting up the dispatcher for easier access
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO) # setting up the logger
 
@@ -28,7 +46,9 @@ if __name__=="__main__":
     dispatcher.add_handler(start_handler)
 
     # Adding the text message handler
-    msg_handler = CommandHandler(Filters.text, msgHandler)
+    msg_handler = MessageHandler(Filters.text, msgHandler)
     dispatcher.add_handler(msg_handler)
 
+    # Let the polling begin
     updater.start_polling()
+    print("Init done")
